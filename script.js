37f078780a791284231f4223dcb65918161e18c7
shun-uehara2 <124650742+shun-uehara2@users.noreply.github.com>
2023-07-23 01:41:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let victoryCount = localStorage.getItem('victoryCount') ? parseInt(localStorage.getItem('victoryCount')) : 0;
+let defeatCount = localStorage.getItem('defeatCount') ? parseInt(localStorage.getItem('defeatCount')) : 0;
+
+document.getElementById('message').textContent = `${victoryCount} - ${defeatCount}`;
+
+document.getElementById('victory').addEventListener('click', function() {
+  victoryCount += 1;
+  localStorage.setItem('victoryCount', victoryCount);
+  document.getElementById('message').textContent = `${victoryCount} - ${defeatCount}`;
+});
+
+document.getElementById('defeat').addEventListener('click', function() {
+  defeatCount += 1;
+  localStorage.setItem('defeatCount', defeatCount);
+  document.getElementById('message').textContent = `${victoryCount} - ${defeatCount}`;
+});
